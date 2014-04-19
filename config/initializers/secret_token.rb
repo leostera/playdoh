e3c006fb9158a932e47f2b518949795d0b14e2cf
Leandro Ostera <me@leostera.com>
2014-04-19 23:15:01 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Playdoh::Application.config.secret_key_base = '6b6c628e77fc7f3a276be96029c2e7d2e570791021fb1dfba136432084738b80743da5b3aac4d59a2656b20fc617267a9ee34a6a257720105c460b03378ff630'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Playdoh::Application.config.session_store :cookie_store, key: '_playdoh_session'
